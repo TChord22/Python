94513fbdb4babdeb4223922e3cffaf3d8bd39a25
Tasha <webdev123321@gmail.com>
2022-08-20 11:20:16 +0200

    v2

--- Day 1/band-generator.py
+++ Day 1/band-generator.py
@@ -1,4 +1,16 @@
-#This is for a band generator
-print('Welcome to the band generator');
-print('Please fill in the details below');
+#Exercise 1 String manipulation
 
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the" + " " + "sign.")
+# print('e.g. print("Hello" + "world")')
+# print("New lines can be \ncreated with a backslash and n.")
+
+#Exercise 2 > User data
+# The input is what gathers the users name
+# The print hello is added to follow through as prompt has been answered
+# print("Hello " + input("What is your name? ")) #Started conversation
+
+#Exercise 3 > Word Counting
+# print(len('Tasha'))
+name = input('What is your name? ')
+print("Your name count is ", len(name))
