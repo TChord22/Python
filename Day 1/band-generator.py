b9704eee64a530a2504a465d13fb0f29760be59e
Tasha <webdev123321@gmail.com>
2022-08-20 11:45:55 +0200

    v3

--- Day 1/band-generator.py
+++ Day 1/band-generator.py
@@ -5,12 +5,29 @@
 # print('e.g. print("Hello" + "world")')
 # print("New lines can be \ncreated with a backslash and n.")
 
+##############################################################################
+
 #Exercise 2 > User data
+
 # The input is what gathers the users name
 # The print hello is added to follow through as prompt has been answered
-# print("Hello " + input("What is your name? ")) #Started conversation
+
+# name = input("What is your name? ")
+# print("Hello " + name) #Started conversation
+# input("How are you? ")
+# print("That is nice to hear " + name + " I am doing better than you are right now")
+# input("Ask me why? ")
+# print("Well " + name + " I get to spend time with you of course!")
+
+############################################################################
 
 #Exercise 3 > Word Counting
+
 # print(len('Tasha'))
-name = input('What is your name? ')
-print("Your name count is ", len(name))
+
+#First try with variable name
+# name = input('What is your name? ') #variable
+# print("Your name count is ", len(name))
+#Second try without variable
+# print("Your name count is", len( input("What is your name? ")))
+
